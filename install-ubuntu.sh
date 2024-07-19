850fab666f681becae7f2ba83e741e1d025b8fa7
Frcs6 <frcs6b@gmail.Com>
2024-07-18 21:00:20 -0400

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-update_system() {
-    sudo apt update
-    sudo apt upgrade -y
-    reboot
-}
-
 install_google_chrome() {
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo apt install ./google-chrome-stable_current_amd64.deb -y
@@ -33,12 +27,12 @@ install_yoga_drivers() {
 }
 
 install_softwares() {
-    sudo apt autoremove gnome-snapshot
+    sudo apt autoremove gnome-snapshot -y
     sudo apt install ubuntu-restricted-addons gnome-tweaks gnome-shell-extension-alphabetical-grid gnome-shell-extension-manager gnome-shell-extension-prefs gnome-shell-extensions timeshift samba cheese papirus-icon-theme -y
     
     snap install spotify jdownloader2
     
-    flatpak install flathub org.keepassxc.KeePassXC flathub com.github.tchx84.Flatseal
+    flatpak install flathub org.keepassxc.KeePassXC flathub com.github.tchx84.Flatseal -y
 }
 
 cleanup_software() {
@@ -46,13 +40,11 @@ cleanup_software() {
 }
 
 configure_gnome() {
-    "snap" >> ~/.hidden
+    'snap' >> ~/.hidden
 }
 
 mkdir ~/tmp
 pushd ~/tmp
-
-#update_system
 
 sudo apt update
 #install_google_chrome
@@ -62,8 +54,6 @@ sudo apt update
 #install_softwares
 #cleanup_software
 #configure_gnome
-
-#reboot
 
 popd
 rm -rf ~/tmp
