717a22c7d805d76cc0f55ba80556f319bdd4a514
Frcs6 <frcs6b@gmail.Com>
2024-12-06 11:18:03 -0500

    Update install-fedora.sh

--- install-fedora.sh
+++ install-fedora.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-install_google_chrome() {
-    echo "[google-chrome]
-	name=google-chrome
-	baseurl=https://dl.google.com/linux/chrome/rpm/stable/x86_64
-	enabled=1
-	gpgcheck=1
-	gpgkey=https://dl.google.com/linux/linux_signing_key.pub" 2>/dev/null > /etc/yum.repos.d/google-chrome.repo
-	sed -e 's/\t//g' -i /etc/yum.repos.d/google-chrome.repo
-    dnf install google-chrome-stable -y
-}
-
 install_dropbox() {
     wget -O dropbox.rpm https://www.dropbox.com/download?dl=packages/fedora/nautilus-dropbox-2024.04.17-1.fc39.x86_64.rpm
     dnf install ./dropbox.rpm -y
@@ -33,12 +22,10 @@ dnf_config() {
 
 clean_repos() {
     rm -f /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:phracek:PyCharm.repo
-    rm -f /etc/yum.repos.d/google-chrome.repo
 }
 
 rpm_fusion() {
-    sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-    sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
+    dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 }
 
 install_softwares() {
@@ -53,15 +40,12 @@ install_softwares() {
     dnf install -y gstreamer1-plugins-ugly
     dnf install -y libdvdcss
     dnf install -y gstreamer1-plugin-openh264
-    dnf install -y adwaita-qt5
-    dnf install -y adwaita-qt6
     dnf install -y gnome-extensions-app
     dnf install -y gnome-shell-extension-dash-to-dock
     dnf install -y gnome-shell-extension-appindicator
     dnf install -y gnome-shell-extension-user-theme
     dnf install -y gnome-tweaks
     dnf install -y file-roller
-    dnf install -y git
     dnf install -y keepassxc
     dnf install -y timeshift
     flatpak install com.github.tchx84.Flatseal -y
@@ -72,12 +56,12 @@ cleanup_software() {
 }
 
 install_yoga_drivers() {
-    sudo dnf -y install git dkms kernel-devel
+    dnf -y install git dkms kernel-devel
     mkdir ~/Drivers
     pushd ~/Drivers
     git clone https://github.com/morrownr/8821au-20210708.git
     pushd ./8821au-20210708
-    sudo ./install-driver.sh
+    ./install-driver.sh
     popd
     popd
 }
@@ -87,7 +71,6 @@ pushd ~/tmp
 
 #dnf_config
 #clean_repos
-#install_google_chrome
 #rpm_fusion
 #install_softwares
 #cleanup_software
