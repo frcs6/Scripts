9af4b3ac5827c15e435980fc04b8f65b6ffe77c5
Frcs6 <frcs6b@gmail.Com>
2025-01-08 19:59:04 -0500

    Create ln-insync-onedrive.sh

---
+++ ln-insync-onedrive.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf ~/Desktop
+ln -s ~/Insync/MAIL/OneDrive/Bureau ~/Desktop
+
+rm -rf ~/Documents
+ln -s ~/Insync/MAIL/OneDrive/Documents ~/Documents
+
+rm -rf ~/Pictures
+ln -s ~/Insync/MAIL/OneDrive/Images ~/Pictures
+
+rm -rf ~/Templates
+ln -s ~/Insync/MAIL/OneDrive/Templates ~/Templates
