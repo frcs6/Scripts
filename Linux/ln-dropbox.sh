44fc10999aa7f00417ec64d0b7126ffefe63350b
Frcs6 <frcs6b@gmail.com>
2025-01-15 20:13:01 -0500

    Fix ln

--- Linux/ln-dropbox.sh
+++ Linux/ln-dropbox.sh
@@ -3,7 +3,9 @@
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 source "$SCRIPT_DIR/common-functions.sh"
 
-create_symlink ~/Dropbox/Desktop ~/Desktop
-create_symlink ~/Dropbox/Documents ~/Documents
-create_symlink ~/Dropbox/Pictures ~/Pictures
-create_symlink ~/Dropbox/Templates ~/Templates
+FOLDER=$1
+
+create_symlink ~/Dropbox/$FOLDER/Desktop ~/Desktop
+create_symlink ~/Dropbox/$FOLDER/Documents ~/Documents
+create_symlink ~/Dropbox/$FOLDER/Pictures ~/Pictures
+create_symlink ~/Dropbox/$FOLDER/Templates ~/Templates
--- Linux/ln-insync-onedrive.sh
+++ Linux/ln-insync-onedrive.sh
@@ -5,7 +5,7 @@ source "$SCRIPT_DIR/common-functions.sh"
 
 MAIL=$1
 
-create_symlink "~/Insync/$MAIL/OneDrive/Bureau" ~/Desktop
-create_symlink "~/Insync/$MAIL/OneDrive/Documents" ~/Documents
-create_symlink "~/Insync/$MAIL/OneDrive/Images" ~/Pictures
-create_symlink "~/Insync/$MAIL/OneDrive/Templates" ~/Templates
+create_symlink ~/Insync/$MAIL/OneDrive/Bureau ~/Desktop
+create_symlink ~/Insync/$MAIL/OneDrive/Documents ~/Documents
+create_symlink ~/Insync/$MAIL/OneDrive/Images ~/Pictures
+create_symlink ~/Insync/$MAIL/OneDrive/Templates ~/Templates
