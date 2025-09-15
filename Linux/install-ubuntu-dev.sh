ae8a8e6a3648f04ba2cd26d690e18d03d97fe7a8
Frcs6 <frcs6b@gmail.com>
2025-09-15 08:17:18 -0400

    Fix office and dev

--- Linux/install-ubuntu-dev.sh
+++ Linux/install-ubuntu-dev.sh
@@ -8,10 +8,10 @@ pushd ~/tmp
 
 sudo snap install --classic code
 
-# sudo add-apt-repository ppa:dotnet/backports
-# sudo apt update
-# sudo apt install dotnet-sdk-8.0 -y
-# sudo apt install dotnet-sdk-9.0 -y
+sudo add-apt-repository ppa:dotnet/backports
+sudo apt update
+sudo apt install dotnet-sdk-8.0 -y
+sudo apt install dotnet-sdk-9.0 -y
 
 popd
 rm -rf ~/tmp
