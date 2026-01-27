f278c7df8b227ddbe6974766e137d797ad920031
Frcs6 <frcs6b@gmail.com>
2026-01-26 21:03:18 -0500

    Mint

---
+++ Linux/install-mint.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+source "$SCRIPT_DIR/common-functions.sh"
+
+sudo apt update
+sudo apt full-upgrade -y
+if command -v snap >/dev/null 2>&1; then
+    sudo snap refresh
+fi
+if command -v flatpak >/dev/null 2>&1; then
+    flatpak update -y
+fi
+
+apt_install curl
+apt_install libfuse2t64
+apt_install samba
+apt_install spotify
+apt_install ttf-mscorefonts-installer
+apt_install ubuntu-restricted-addons
+apt_install ubuntu-restricted-extras
+apt_install wget
+apt_install gstreamer1.0-plugins-base
+apt_install gstreamer1.0-plugins-good
+apt_install gstreamer1.0-plugins-bad
+apt_install gstreamer1.0-plugins-ugly
+apt_install gstreamer1.0-libav
+apt_install vlc
+install_google_chrome
+flatpak_install org.keepassxc.KeePassXC
+
+# Nettoyage final
+sudo apt autoremove --purge -y
+sudo apt autopurge -y
+sudo apt autoremove -y
+if command -v flatpak >/dev/null 2>&1; then
+    flatpak uninstall --unused -y
+fi
