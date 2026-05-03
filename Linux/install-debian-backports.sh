8f190de9e46a0f6e3df2463c9bdc63c9727b750a
Frcs6 <frcs6b@gmail.com>
2026-05-02 23:17:00 -0400

    Fix

--- Linux/install-debian-backports.sh
+++ Linux/install-debian-backports.sh
@@ -2,5 +2,5 @@
 
 sudo apt update
 
-sudo apt -t bookworm-backports install linux-image-amd64 -y
-sudo apt -t bookworm-backports install libgl1-mesa-dri libglx-mesa0 mesa-vulkan-drivers -y
+sudo apt -t trixie-backports install linux-image-amd64 -y
+sudo apt -t trixie-backports install libgl1-mesa-dri libglx-mesa0 mesa-vulkan-drivers -y
