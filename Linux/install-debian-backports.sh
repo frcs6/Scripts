c9b26658fba87770c094a3a0c88ff0404a4ae3de
Frcs6 <frcs6b@gmail.com>
2026-05-04 10:26:00 -0400

    Fix

--- Linux/install-debian-backports.sh
+++ Linux/install-debian-backports.sh
@@ -7,17 +7,22 @@ sudo apt -t trixie-backports install linux-image-amd64 -y
 # ThinkPad E16
 # sudo apt -t trixie-backports install \
 #   libgl1-mesa-dri libglx-mesa0 mesa-vulkan-drivers \
-#   firmware-amd-graphics libva2 libgl1-mesa-glx \
+#   firmware-amd-graphics libva2 libgl1 \
 #   mesa-va-drivers mesa-vdpau-drivers -y
 
 # Yoga 13
 # sudo apt -t trixie-backports install \
-#   libgl1-mesa-dri libglx-mesa0 libgl1-mesa-glx \
+#   libgl1-mesa-dri libglx-mesa0 libgl1 \
 #   intel-media-va-driver-non-free libva2 \
 #   mesa-va-drivers mesa-vdpau-drivers -y
 
 # Aspire 5
 # sudo apt -t trixie-backports install \
 #   libgl1-mesa-dri libglx-mesa0 mesa-vulkan-drivers \
-#   intel-media-va-driver-non-free libva2 libgl1-mesa-glx \
+#   intel-media-va-driver-non-free libva2 libgl1 \
 #   mesa-va-drivers mesa-vdpau-drivers -y
+
+# VMware Workstation
+# sudo apt -t trixie-backports install \
+#   libgl1-mesa-dri libglx-mesa0 libgl1 \
+#   mesa-utils open-vm-tools-desktop -y
