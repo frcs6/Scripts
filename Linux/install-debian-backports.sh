0b0ce9f9b6625f4a3394bab0fe74fa90e38885fc
Frcs6 <frcs6b@gmail.com>
2026-05-06 12:05:30 -0400

    Fix

--- Linux/install-debian-backports.sh
+++ Linux/install-debian-backports.sh
@@ -8,21 +8,25 @@ sudo apt -t trixie-backports install linux-image-amd64 -y
 # sudo apt -t trixie-backports install \
 #   libgl1-mesa-dri libglx-mesa0 mesa-vulkan-drivers \
 #   firmware-amd-graphics libva2 libgl1 \
-#   mesa-va-drivers mesa-vdpau-drivers -y
+#   mesa-va-drivers mesa-vdpau-drivers \
+#   libegl-mesa0 vulkan-tools -y
 
 # Yoga 13
 # sudo apt -t trixie-backports install \
 #   libgl1-mesa-dri libglx-mesa0 libgl1 \
 #   intel-media-va-driver-non-free libva2 \
-#   mesa-va-drivers mesa-vdpau-drivers -y
+#   mesa-va-drivers mesa-vdpau-drivers \
+#   libegl-mesa0 vulkan-tools -y
 
 # Aspire 5
 # sudo apt -t trixie-backports install \
 #   libgl1-mesa-dri libglx-mesa0 mesa-vulkan-drivers \
 #   intel-media-va-driver-non-free libva2 libgl1 \
-#   mesa-va-drivers mesa-vdpau-drivers -y
+#   mesa-va-drivers mesa-vdpau-drivers \
+#   libegl-mesa0 vulkan-tools -y
 
 # VMware Workstation
 # sudo apt -t trixie-backports install \
 #   libgl1-mesa-dri libglx-mesa0 libgl1 \
-#   mesa-utils open-vm-tools-desktop -y
+#   mesa-utils open-vm-tools-desktop \
+#   libegl-mesa0 vulkan-tools -y
